e8343c28c91bcf1b51491b90551ba7ea02ebf8d8
Tanman4ButCooler <107323433+Tanman4ButCooler@users.noreply.github.com>
2026-02-19 12:12:49 -0700

    Fixed folders

--- Calc.cs
+++
@@ -1,9 +0,0 @@
-// making a simple calculation
-public class Calc {
-    public static int add(int a, int b) {
-        console.log("Adding " + a + " and " + b);
-        return a + b;
-    }
-}
-
-// lol sorry for the mess
--- Java.js
+++
@@ -1,7 +0,0 @@
-// collector for Java files
-MediaSourceHandle.exports = function(mediaSource, filePath) {
-    if (filePath.endsWith('.java')) {
-        mediaSource.addTag('language', 'java');
-    }
-    return mediaSource;
-};
